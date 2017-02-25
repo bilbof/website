4c6898915472b80162bd14281694b243989300fb
Bill Franklin <b.franklin@outlook.com>
2017-02-25 16:41:04 +0000

    hotfix

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,5 @@
 var express     = require('express');
 var router      = express.Router();
-var gameEvents  = require('../gameEvents.js');
 
 /* GET game */
 router.get('/', function(req, res, next){
